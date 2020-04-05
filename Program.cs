fc748ed8331094690468b265d03e90e3acc7acb1
Kiril Chobanov <62597135+kiril2000@users.noreply.github.com>
2020-04-05 16:13:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Even2Multiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            if (n % 2 != 0)
+                n = n * 2;
+            else
+                Console.WriteLine("This is odd number!");
+
+            Console.WriteLine(n);
+        }
+    }
+}
